dea1f45c4dcffde0481ebd41b3c8d7f09b16a6d2
Igor-Brandalise <brandaliseigor123@gmail.com>
2025-07-07 23:08:16 -0300

    first commit

---
+++ src/server.ts
@@ -0,0 +1,22 @@
+import { fastify } from "fastify";
+
+import {
+  serializerCompiler,
+  validatorCompiler,
+  type ZodTypeProvider,
+} from "fastify-type-provider-zod";
+import { fastifyCors } from "@fastify/cors";
+
+const app = fastify().withTypeProvider<ZodTypeProvider>();
+
+app.register(fastifyCors, {
+  origin: "*",
+});
+
+app.setSerializerCompiler(serializerCompiler);
+app.setValidatorCompiler(validatorCompiler);
+
+app.listen({ port: process.env.PORT ? Number(process.env.PORT) : 3333}).then(() => {
+    console.log(`Port:${process.env.PORT}`)
+  console.log("HTTP server runing!");
+});
