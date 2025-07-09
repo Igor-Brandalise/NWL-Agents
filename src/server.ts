4cd17739930180d20518f646e0485d2bebf41cc9
Igor Brandalise <brandaliseigor123@gmail.com>
2025-07-08 21:58:04 -0300

    install biome.js

--- src/server.ts
+++ src/server.ts
@@ -16,7 +16,13 @@ app.register(fastifyCors, {
 app.setSerializerCompiler(serializerCompiler);
 app.setValidatorCompiler(validatorCompiler);
 
-app.listen({ port: process.env.PORT ? Number(process.env.PORT) : 3333}).then(() => {
-    console.log(`Port:${process.env.PORT}`)
-  console.log("HTTP server runing!");
+app.get("/health", () => {
+  return "OK";
 });
+
+app
+  .listen({ port: process.env.PORT ? Number(process.env.PORT) : 3333 })
+  .then(() => {
+    console.log(`Port:${process.env.PORT}`);
+    console.log("HTTP server runing!");
+  });
